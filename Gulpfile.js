bd396ffa43ddbd2953671851bf718c5e06f3451b
suisho <suisho000000@gmail.com>
2015-01-28 16:26:55 +0900

    susy?

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//var gutil = require('gulp-util');
+
 gulp.task('sass', function () {
   gulp.watch("scss/**/*.scss", function(e){
     gulp.src('scss/**/*.scss')
-      .pipe(sass())
+      .pipe(sass().on("error", function(e){console.log(e)} ))
       .pipe(gulp.dest('./css/'));
+    console.log("End")
   })
 })
 gulp.task('default', ['sass'])
