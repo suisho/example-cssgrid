64bdb411327287d9e5594aba550e21a953318566
suisho <suisho000000@gmail.com>
2015-02-02 19:06:23 +0900

    jadize

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var jade = require('gulp-jade')
+//console.log(jade)
+/*
+gulp.task('template', function () {
+  //gulp.watch("index.jade", function(e){
+    gulp.src('index.jade')
+      .pipe(jade({}))
+      .pipe(gulp.dest("index.html"))
+  //})
+})
+*/
 gulp.task('sass', function () {
   gulp.watch("scss/**/*.scss", function(e){
     gulp.src('scss/**/*.scss')
       .pipe(sass().on("error", function(e){console.log(e)} ))
       .pipe(gulp.dest('./css/'));
-    console.log("End")
   })
 })
-gulp.task('default', ['sass'])
+
+gulp.task('default', ['sass', 'template'])
+
+
