769d40038145a22601bb12ae916592d043546866
suisho <suisho000000@gmail.com>
2015-02-02 20:07:12 +0900

    add jade

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,23 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var jade = require('gulp-jade')
-//console.log(jade)
-/*
-gulp.task('template', function () {
-  //gulp.watch("index.jade", function(e){
-    gulp.src('index.jade')
-      .pipe(jade({}))
-      .pipe(gulp.dest("index.html"))
-  //})
+gulp.task('watch', function () {
+  gulp.watch('*.jade',["template"])
+  gulp.watch('scss/**/*.scss',["sass"])
 })
-*/
+gulp.task('template', function () {
+  gulp.src('index.jade')
+    .pipe(jade({
+      pretty: true
+    }).on("error", function(e){console.log(e)} ))
+    .pipe(gulp.dest("./"))
+})
 gulp.task('sass', function () {
-  gulp.watch("scss/**/*.scss", function(e){
-    gulp.src('scss/**/*.scss')
-      .pipe(sass().on("error", function(e){console.log(e)} ))
-      .pipe(gulp.dest('./css/'));
-  })
+  gulp.src('scss/**/*.scss')
+    .pipe(sass().on("error", function(e){console.log(e)} ))
+    .pipe(gulp.dest('./css/'));
 })
 
-gulp.task('default', ['sass', 'template'])
+gulp.task('default', ['sass', 'template', 'watch'])
 
 
